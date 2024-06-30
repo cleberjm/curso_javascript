9f8cfe0f0b8b100cb850e76b968ba183a9404d35
cleber <machadex@yahoo.com.br>
2024-06-29 23:51:17 -0300

    feat: aula 07

---
+++ aula-07.js
@@ -0,0 +1,12 @@
+//array []
+const idades = [18, 14, 53, 1, 5, 87, 90, 65];
+
+//Pegar dado pelo indice do array, começa sempre em 0
+console.log('Resultado:', idades[5]);
+
+//Verificar a quantidade de elementos em um array
+console.log('Resultado:', idades.length)
+
+for(let i=0; i < idades.length; i++) {
+    console.log('Resultado:', idades[i]);
+}

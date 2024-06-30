2071ac09c7cee00535c1bffb12f9d8f41459bce2
cleber <machadex@yahoo.com.br>
2024-06-29 23:16:10 -0300

    feat: aula-03

---
+++ aula-03.js
@@ -0,0 +1,9 @@
+//Operadores OU || E &&, '=' recebe, '==' igual com conversão, '===' igual sem conversão
+const idade = 8;
+const nome = 'Cleber'
+
+if(idade >= 18) {
+    console.log('Acesso permitido!')
+} else {
+    console.log('Acesso negado!')
+}

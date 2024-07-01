645c18805257dc743ea39e75201f619cdc015676
cleber <machadex@yahoo.com.br>
2024-07-01 14:16:54 -0300

    feat: aula 08

--- aula-07.js
+++ aula-07.js
@@ -2,10 +2,10 @@
 const idades = [18, 14, 53, 1, 5, 87, 90, 65];
 
 //Pegar dado pelo indice do array, começa sempre em 0
-console.log('Resultado:', idades[5]);
+console.log('Valor posição 5:', idades[5]);
 
 //Verificar a quantidade de elementos em um array
-console.log('Resultado:', idades.length)
+console.log('Tamanho Array:', idades.length)
 
 for(let i=0; i < idades.length; i++) {
     console.log('Resultado:', idades[i]);
---
+++ aula-08.js
@@ -0,0 +1,9 @@
+const salario = 103;
+const pagamento = '1200';
+
+//Converte o valor da constante em string e utiliza 2 casas decimais
+console.log('Salário: ', salario.toFixed(2))
+
+//Converte o valor da constante em número e utiliza 2 casas decimais
+console.log('Pagamento:', Number(pagamento).toFixed(2))
+

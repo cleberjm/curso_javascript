fa1736da2fadd21590bee4702811d2b433bd85c4
cleber <machadex@yahoo.com.br>
2024-07-01 14:50:33 -0300

    feat: aula 11

---
+++ aula-11.js
@@ -0,0 +1,13 @@
+//OPERADORES
+// +  Soma
+console.log('Resultado:', (6 + 5));
+// - Subtração
+console.log('Resultado:', (6 - 5));
+// / Divisão
+console.log('Resultado:', (6 / 5));
+// * Multiplicação
+console.log('Resultado:', (6 * 5));
+// % Resto
+console.log('Resultado:', (6 % 5));
+// ** Elevado
+console.log('Resultado:', (6 ** 5));

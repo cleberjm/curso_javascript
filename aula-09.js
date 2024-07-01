f657c6853fe26314b25541f4c66c6fdc5a9de728
cleber <machadex@yahoo.com.br>
2024-07-01 14:23:17 -0300

    feat: aula 09

---
+++ aula-09.js
@@ -0,0 +1,12 @@
+//Criando função
+
+function soma(item1, item2) {
+    return item1 + item2;
+}
+
+function subtrair(item1, item2) {
+    return item1 - item2;
+}
+
+console.log('Soma:', soma(5, 6))
+console.log('Subtrair:', subtrair(45, 60))

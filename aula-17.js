074e0734b1bbef6cea028c731b8644113337a59f
cleber <machadex@yahoo.com.br>
2024-07-01 23:28:27 -0300

    feat: aula 17

---
+++ aula-17.js
@@ -0,0 +1,20 @@
+//Switch Case
+
+function teste(key) {
+    switch (key) {
+        case 'Cleber':
+            console.log(`${key} aqui`)
+            break;
+        case 'Bento':
+            console.log(`${key} aqui`)
+        break;
+        case 'Clara':
+            console.log(`${key} aqui`)
+        break;
+        default:
+            console.log('Outro aqui')
+            break;
+    }
+}
+
+teste('Cleber');

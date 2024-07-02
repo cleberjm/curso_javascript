a582b3abbe3b5d09825ecd64a298f17892066779
cleber <machadex@yahoo.com.br>
2024-07-02 15:40:38 -0300

    feat: aula 19

---
+++ aula-19.js
@@ -0,0 +1,14 @@
+//Tipos de declaração de função
+function Calcular(){
+    console.log('Calcular');
+}
+
+const Calcular2 = function(){
+    console.log('Calcular 2');
+}
+
+const Calcular3 = () => console.log('Calcular 3')
+
+Calcular();
+Calcular2();
+Calcular3();

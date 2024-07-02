aaeac5d213af5da073aa3efcd0ad4dda5ae2ac34
cleber <machadex@yahoo.com.br>
2024-07-01 23:09:56 -0300

    feat: aula 15

--- aula-08.js
+++ aula-08.js
@@ -4,6 +4,6 @@ const pagamento = '1200';
 //Converte o valor da constante em string e utiliza 2 casas decimais
 console.log('Salário: ', salario.toFixed(2))
 
-//Converte o valor da constante em número e utiliza 2 casas decimais
+//Converte o valor da constante em número, depois converte em string e utiliza 2 casas decimais
 console.log('Pagamento:', Number(pagamento).toFixed(2))
 

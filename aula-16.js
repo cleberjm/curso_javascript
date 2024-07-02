ba3fc7be18d459afe62f26f816aaf839c4c1468c
cleber <machadex@yahoo.com.br>
2024-07-01 23:17:34 -0300

    feat: aula 16

---
+++ aula-16.js
@@ -0,0 +1,11 @@
+//Tipos Primitivos: numero, string e booleano
+const numero = 4324;
+const stringA = 'bla';
+const verdadeOuFalse = true; //false
+
+const arrayTest = [10, 20, 30];
+
+//Inserir dado array
+arrayTest.push(4774);
+
+console.log(arrayTest)

60ce052dd0289e0f1be6ab9a211f0621cebfd143
cleber <machadex@yahoo.com.br>
2024-07-08 23:37:19 -0300

    feat: aula 34

---
+++ aula-34.js
@@ -0,0 +1,30 @@
+//Criando classe
+
+class Pessoa {
+    nome;
+    idade;
+    filhos;
+
+    quantosFilhos() {
+        if(this.filhos) {
+            return this.filhos.length;
+        }
+        return 0;
+    }
+
+    constructor(nome, idade, filhos) {
+        this.nome = nome;
+        this.idade = idade;
+        this.filhos = filhos;
+    }
+}
+
+const filho1 = new Pessoa('Bento', 6);
+
+const filho2 = new Pessoa('Clara', 0)
+
+const pessoa = new Pessoa('Cleber', 42, [filho1, filho2]);
+
+console.log(pessoa);
+
+console.log('Quantidade de filhos: ', pessoa.quantosFilhos())
